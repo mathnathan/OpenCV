da8ab57b442a7fa0701cab397b66b0dba585b545
mathnathan <mathnathan@gmail.com>
2010-07-14 21:09:26 -0400

    edit for blog

--- examples/example2-5.cpp
+++ examples/example2-5.cpp
@@ -1,8 +1,10 @@
 # include "stdio.h"
+# include "cv.h"
 # include "highgui.h"
 
 int main( int argc, char** argv ) {
 	IplImage* frame = 0;
+	IplImage* augframe = 0;
 
 	if( argc < 2 ) {
 		printf( "Usage: Accepts one video as argument\n" );
@@ -11,13 +13,17 @@ int main( int argc, char** argv ) {
 
 	cvNamedWindow( "Example2", CV_WINDOW_AUTOSIZE );
 	cvMoveWindow( "Example2", 100, 100);
+	cvNamedWindow( "output", CV_WINDOW_AUTOSIZE );
+	cvMoveWindow( "output", 500, 100);
 	CvCapture* capture = cvCreateFileCapture( argv[1] );
 
 	while(1) {
 	    frame = cvQueryFrame( capture );
 	    if( !frame) break;
+		augframe = cvCreateImage( cvGetSize( frame ), IPL_DEPTH_8U, 3 );
 		cvSmooth( frame, augframe, CV_BLUR, 3, 3 );
 	    cvShowImage( "Example2", frame );
+	    cvShowImage( "output", augframe );
 	    char c = cvWaitKey(73);
 	    if( c == 27 ) break;
 	}
